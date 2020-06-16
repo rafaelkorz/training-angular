bd4fe63d9c03ee09932053cd3ff002c586b461f2
Rafael Korz <rafael.korz@gmail.com>
2020-06-15 22:54:55 -0300

    Diretivas

---
+++ diretivas/src/app/diretiva-ngif/diretiva-ngif.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-diretiva-ngif',
+  templateUrl: './diretiva-ngif.component.html',
+  styleUrls: ['./diretiva-ngif.component.sass']
+})
+export class DiretivaNgifComponent implements OnInit {
+
+  cursos: string[] = ['Angular'];
+
+  mostrarCursos: boolean;
+
+  onMostrarCursos() {
+    this.mostrarCursos = !this.mostrarCursos;
+  }
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
